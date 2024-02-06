e90f028971b251266682bdbc2bfbc876d1bc7c15
Win 10 Pro 21H1 <Win 10 Pro 21H1@DESKTOP-POAAEVP>
2024-02-07 02:44:47 +0700

    Add project files.

---
+++ KYSDotNetTraingBatch3.ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+//Console.ReadLine ();
+#region Read
+
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+sqlConnectionStringBuilder.DataSource = ".";
+sqlConnectionStringBuilder.InitialCatalog = "TestDb";
+sqlConnectionStringBuilder.UserID = "sa";
+sqlConnectionStringBuilder.Password = "koyeswan@8888";
+
+string query = "select * from tbl_blog";
+SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+sqlConnection.Open();
+
+SqlCommand cmd = new SqlCommand(query, sqlConnection );
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+
+DataTable dt = new DataTable();
+adapter.Fill(dt);
+
+
+
+sqlConnection.Close();
+
+foreach(DataRow dr in dt.Rows)
+{
+    Console.WriteLine(dr["BlogId"]);
+    Console.WriteLine(dr["BlogTitle"]);
+    Console.WriteLine(dr["BlogAuthor"]);
+    Console.WriteLine(dr["BlogContent"]);
+}
+
+#endregion
+
+Console.ReadKey();
